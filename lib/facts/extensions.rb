0d39189533b13ef88031e4e3653637d35105a657
Ryan McKern <ryan@orangefort.com>
2014-05-18 23:21:02 -0700

    Ensure LOADPATH is unique after loading Puppet

--- lib/facts/extensions.rb
+++ lib/facts/extensions.rb
@@ -8,12 +8,10 @@ class Facts
       Puppet.parse_config
 
       # If you've set 'vardir' but not 'libdir' in your
-      # puppet.conf, then the hook to add libdir to $:
-      # won't get triggered.  This makes sure that it's setup
-      # correctly.
-      unless $LOAD_PATH.include?(Puppet[:libdir])
-        $LOAD_PATH << Puppet[:libdir]
-      end
+      # puppet.conf, then the hook to add libdir to $: won't
+      # get triggered. This makes sure that it's setup correctly.
+      $:.push Puppet[:libdir] unless $:.include? Puppet[:libdir]
+      $:.uniq!
     rescue LoadError => detail
       $stderr.puts "Could not load Puppet: #{detail}"
     end
