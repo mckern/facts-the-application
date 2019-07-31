286155141de17e2a3cfe19a84ce96896f2f689a3
Ryan McKern <ryan@mckern.sh>
2019-07-31 12:24:15 -0700

    Expand -h output

--- bin/facts-the-application.rb
+++ bin/facts-the-application.rb
@@ -9,19 +9,38 @@ require 'bundler/setup'
 require 'facts'
 
 if ARGV.any?
-  OptionParser.new do |op|
-    op.on('-p', '--port port', 'set the port (default is 4567)') do |val|
-      Facts.set :port, Integer(val)
-    end
+  begin
+    OptionParser.new do |opts|
+      opts.separator("\noptions:")
+      opts.on('-p', '--port port', 'set the port (default is 4567)') do |val|
+        Facts.set :port, Integer(val)
+      end
 
-    op.on('-b', '--bind addr', 'set the host (default is localhost)') do |val|
-      Facts.set :bind, val
-    end
+      opts.on('-b', '--bind addr', 'set the host (default is localhost)') do |val|
+        Facts.set :bind, val
+      end
 
-    op.on('-e', '--environment env', 'set the environment (default is development)') do |val|
-      Facts.set :environment, val.to_sym
-    end
-  end.parse!(ARGV.dup)
+      opts.on('-e', '--environment env', 'set the environment (default is development)') do |val|
+        Facts.set :environment, val.to_sym
+      end
+
+      opts.on_tail('-h', '--help', 'Show this message') do
+        puts "#{opts}\n"
+        puts <<~ENVVARS
+          envrironment variables:
+              DEBUG:               set to any value to print debug messages to stderr.
+              EXPIRY:              number of seconds to cache Facter data for
+              FILTER:              comma delimited list of keys to exclude from output
+              FACTERDIR:           path to Facter 'lib' directory
+              SLOW_FACTER_PLEASE:  use pure Ruby Facter instead of Native Facter
+                                   (Ruby Facter does not have support for structured facts)
+        ENVVARS
+        exit
+      end
+    end.parse!(ARGV.dup)
+  rescue OptionParser::MissingArgument => msg
+    abort "ERROR: #{msg}"
+  end
 end
 
 begin
